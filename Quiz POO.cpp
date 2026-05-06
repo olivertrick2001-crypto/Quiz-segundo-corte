c83b8cb147523df8de885cf5b658834238a94c27
olivertrick2001-crypto <olivertrick200@gmail.com>
2026-05-06 13:56:12 -0500

    Primer commit del quiz

---
+++ Quiz POO.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // Librería para entrada y salida (cout, cin)
+#include <string>    // Librería para usar cadenas de texto (string)
+
+// Clase base (Producto)
+class Product {
+protected:
+    // Atributos protegidos: accesibles en esta clase y en clases hijas
+    std::string name;
+    float price;
+    float quantity;
