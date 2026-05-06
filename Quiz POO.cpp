483e41e26fd468706711ce88dfaed10c7b58217d
olivertrick2001-crypto <olivertrick200@gmail.com>
2026-05-06 15:13:06 -0500

    *Commit #1: *Creación de la clase Product *Implementación de atributos: name, price, quantity

--- Quiz POO.cpp
+++ Quiz POO.cpp
@@ -9,26 +9,4 @@ protected:
     float price;
     float quantity;
  
-    public:
-    // Método para solicitar los datos del producto al usuario
-    void solicitarDatos() {
-        std::cout << "Ingrese el nombre del producto: ";
-        std::cin >> name;  // Captura el nombre (sin espacios)
-
-        std::cout << "Ingrese el precio: ";
-        std::cin >> price; // Captura el precio
-
-        std::cout << "Ingrese la cantidad: ";
-        std::cin >> quantity; // Captura la cantidad
-    }
-
-    // Método para calcular el total (precio * cantidad)
-    float calcularTotal() {
-        return price * quantity;
-    }
-
-    // Método para mostrar el total sin descuento
-    void mostrarTotal() {
-        std::cout << "Total sin descuento: " << calcularTotal() << std::endl;
-    }
-};
+   
